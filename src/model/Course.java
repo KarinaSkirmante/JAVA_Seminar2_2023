c0e9f5f460c3a931bd411b5839233b973483ef38
karina.skirmante <karina.krinkele@venta.lv>
2023-03-10 14:04:29 +0200

    Edit: added getters and setters for Course

--- src/model/Course.java
+++ src/model/Course.java
@@ -8,7 +8,69 @@ public class Course {
 	private CourseGradeType type; //not null
 	private Professor professor; //not null
 	
+	private static long courseCounter = 12000;
+	
 	//2.getters and setters
+	public long getId() {
+		return id;
+	}
+	public void setId() {
+		this.id = courseCounter;
+		courseCounter++;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String inputTitle) {
+		if(inputTitle != null && inputTitle.matches("[A-ZĒŪĪĻĶĢŠĀŽČŅ]{1}[a-zēūīļķģšāžčņA-ZĒŪĪĻĶĢŠĀŽČŅ0-9\\s]+")) {
+			title = inputTitle;
+		}
+		else
+		{
+			title = "Unknown";
+		}
+	}
+	public int getCreditPoints() {
+		return creditPoints;
+	}
+	public void setCreditPoints(int inputCreditPoints) {
+		if(inputCreditPoints > 0 && inputCreditPoints < 21) {
+			creditPoints = inputCreditPoints;
+		}
+		else
+		{
+			creditPoints = 0;
+		}
+	}
+	public CourseGradeType getType() {
+		return type;
+	}
+	public void setType(CourseGradeType inputType) {
+		if(inputType!=null) {
+			type = inputType;
+		}
+		else
+		{
+			type = CourseGradeType.other;
+		}
+	}
+	public Professor getProfessor() {
+		return professor;
+	}
+	public void setProfessor(Professor inputProfessor) {
+		if(inputProfessor != null) {
+			professor = inputProfessor;
+		}
+		else
+		{
+			professor = new Professor();//it will call Professor no-args constructor
+		}
+	}
+	
+
+	
+	
+	
 	//3.constructors
 	//4. toString
 	//5. additional functions
