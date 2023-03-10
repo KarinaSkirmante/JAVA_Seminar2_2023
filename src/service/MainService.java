1fbe9327b3d5a3147953b48a25f74e35933268db
karina.skirmante <karina.krinkele@venta.lv>
2023-03-10 14:59:22 +0200

    Edit: Mainservice - added tests fro Grade objects

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -4,6 +4,7 @@ import model.Course;
 import model.CourseGradeType;
 import model.Degree;
 import model.Faculty;
+import model.Grade;
 import model.Professor;
 import model.Student;
 
@@ -15,7 +16,7 @@ public class MainService {
 		
 		Student st2 = new Student("Jānis", "Bērziņš", Faculty.ITF, "121200-12345");
 		System.out.println(st2);
-		Student st3 = new Student("Jā242nis", "Bē$%rziņš", Faculty.ITF, "121x00-12345");
+		Student st3 = new Student("Līga", "Jaukā", Faculty.ITF, "121400-12345");
 		System.out.println(st3);
 		
 		Professor pr1 = new Professor();
@@ -36,6 +37,20 @@ public class MainService {
 		
 		Course c4 = new Course("Datu Struktūras un pamatalgoritmi", 2, CourseGradeType.EXAM, pr2);
 		System.out.println(c4);
+		
+		Grade gr1 = new Grade();
+		System.out.println(gr1);
+		
+		Grade gr2 = new Grade(10, st2, c2);
+		System.out.println(gr2);
+		Grade gr3 = new Grade(6, st3, c2);
+		System.out.println(gr3);
+		Grade gr4  = new Grade(5, st2, c3);
+		System.out.println(gr4);
+		
+		
+		
+		
 	}
 
 }
